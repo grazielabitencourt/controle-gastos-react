198a22652451316d98db4f9cec8d8d8f1c97762b
Graziela Bitencourt <grazielabitencourt59@gmail.com>
2021-10-27 21:55:45 -0300

    Passando uma funcao para o componente filho

--- README.md
+++
@@ -1,2 +0,0 @@
-# controle-gastos-react
-Curso Rocketseats com objetivo de fazer controle de gastos.
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'
 import { App } from './App';
 
+createServer({ 
+   //routes: quais sao as rotas que vou ter na minha api fictícia 
+  routes() {
+    this.namespace = 'api'; // pq está a partir desse endereço.
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
