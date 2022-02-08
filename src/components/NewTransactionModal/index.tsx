4930b5f98152c4f19e8a462633ee1cb175fb6674
Graziela Bitencourt <grazielabitencourt59@gmail.com>
2022-02-08 17:51:02 -0300

    Calculando resumo e criando hook

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useState, useContext } from 'react';
 import Modal from 'react-modal';
-import { TransactionsContext } from '../../TransactionsContext';
+import { TransactionsContext, useTransactions } from '../../hooks/useTransactions';
 import { api } from '../../services/api';
 
 import incomeImg from '../../assets/income.svg';
@@ -18,7 +18,7 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
 
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState(0);
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,11 @@
 import { useContext } from "react";
-import { TransactionsContext } from "../../TransactionsContext";
+import { TransactionsContext, useTransactions } from "../../hooks/useTransactions";
 import { Container } from "./styles";
 
 
 
 export function TransactionsTable() {
-  const { transactions } = useContext(TransactionsContext);
+  const { transactions } = useTransactions();
 
   return (
     <Container>
